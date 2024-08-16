4feb54ae86400c924f688455906167e79137fb5d
jungjaewon <woodcook486@naver.com>
2024-08-16 21:48:33 +0900

    fix error about config

--- train.py
+++ train.py
@@ -84,32 +84,32 @@ def evaluate(config, epoch, pipeline):
     # Sample some images from random noise (this is the backward diffusion process).
     # The default pipeline output type is `List[PIL.Image]`
     images = pipeline(
-        batch_size=config.eval_batch_size,
-        generator=torch.Generator(device='cpu').manual_seed(config.seed), # Use a separate torch generator to avoid rewinding the random state of the main training loop
+        batch_size=config['eval_batch_size'],
+        generator=torch.Generator(device='cpu').manual_seed(config['seed']), # Use a separate torch generator to avoid rewinding the random state of the main training loop
     ).images
 
     # Make a grid out of the images
     image_grid = make_image_grid(images, rows=4, cols=4)
 
     # Save the images
-    test_dir = os.path.join(config.output_dir, "samples")
+    test_dir = os.path.join(config['output_dir'], "samples")
     os.makedirs(test_dir, exist_ok=True)
     image_grid.save(f"{test_dir}/{epoch:04d}.png")
 
 def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
     # Initialize accelerator and tensorboard logging
     accelerator = Accelerator(
-        mixed_precision=config.mixed_precision,
-        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        mixed_precision=config['mixed_precision'],
+        gradient_accumulation_steps=config['gradient_accumulation_steps'],
         #log_with="tensorboard",
-        project_dir=os.path.join(config.output_dir, "logs"),
+        project_dir=os.path.join(config['output_dir'], "logs"),
     )
     if accelerator.is_main_process:
-        if config.output_dir is not None:
-            os.makedirs(config.output_dir, exist_ok=True)
-        if config.push_to_hub:
+        if config['output_dir'] is not None:
+            os.makedirs(config['output_dir'], exist_ok=True)
+        if config['push_to_hub']:
             repo_id = create_repo(
-                repo_id=config.hub_model_id or Path(config.output_dir).name, exist_ok=True
+                repo_id=config['hub_model_id'] or Path(config['output_dir']).name, exist_ok=True
             ).repo_id
         accelerator.init_trackers("train_example")
 
@@ -123,7 +123,7 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
     global_step = 0
 
     # Now you train the model
-    for epoch in range(config.num_epochs):
+    for epoch in range(config['num_epochs']):
         progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
         progress_bar.set_description(f"Epoch {epoch}")
 
@@ -135,7 +135,7 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
 
             # Sample a random timestep for each image
             timesteps = torch.randint(
-                0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device,
+                0, noise_scheduler.config['num_train_timesteps'], (bs,), device=clean_images.device,
                 dtype=torch.int64
             )
 
@@ -165,19 +165,19 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
         if accelerator.is_main_process:
             pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
 
-            if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
+            if (epoch + 1) % config['save_image_epochs'] == 0 or epoch == config['num_epochs'] - 1:
                 evaluate(config, epoch, pipeline)
 
-            if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
-                if config.push_to_hub:
+            if (epoch + 1) % config['save_model_epochs'] == 0 or epoch == config['num_epochs'] - 1:
+                if config['push_to_hub']:
                     upload_folder(
                         repo_id=repo_id,
-                        folder_path=config.output_dir,
+                        folder_path=config['output_dir'],
                         commit_message=f"Epoch {epoch}",
                         ignore_patterns=["step_*", "epoch_*"],
                     )
                 else:
-                    pipeline.save_pretrained(config.output_dir)
+                    pipeline.save_pretrained(config['output_dir'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -203,10 +203,10 @@ if __name__ == '__main__':
     else:
         dataset = get_loader(config)
 
-    train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+    train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config['train_batch_size'], shuffle=True)
 
     model = UNet2DModel(
-        sample_size=config.image_size,  # the target image resolution
+        sample_size=config['image_size'],  # the target image resolution
         in_channels=3,  # the number of input channels, 3 for RGB images
         out_channels=3,  # the number of output channels
         layers_per_block=2,  # how many ResNet layers to use per UNet block
